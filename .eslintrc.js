78ce955d2fa845b909906801e62507e17377c519
James Sral <jamessral@gmail.com>
2018-05-04 11:22:52 -0400

    Add react router and stub basic structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   'plugins': ['react'],
   'rules': {
     'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx'] }],
+    'jsx-a11y/anchor-is-valid': 0,
   },
   'globals': {
     'document': true,
--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div>
+    MealPlanner
+  </div>
+);
+
+const Ingredients = () => (
+  <div>
+    Ingredients
+  </div>
+);
+
+const Recipes = () => (
+  <div>
+    Recipes
+  </div>
+);
+
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">MealPlanner</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      <ul>
+        <li>
+          <Link to="/ingredients">Ingredients</Link>
+        </li>
+        <li>
+          <Link to="/recipes">Recipes</Link>
+        </li>
+      </ul>
+
+      <Route exact path="/" component={Home} />
+      <Route path="/ingredients" component={Ingredients} />
+      <Route path="/recipes" component={Recipes} />
+    </div>
+  </Router>
+);
 
 export default App;
