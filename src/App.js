3a148fdf7fa749a62781fbcd658d927f97f782fa
James Sral <jamessral@gmail.com>
2018-05-04 11:45:43 -0400

    Add basic tailwind styles

--- src/App.js
+++ src/App.js
@@ -26,28 +26,31 @@ const Recipes = () => (
 );
 
 const App = () => (
-  <Router>
-    <div className="text-center">
-      <header className="App-header">
-        <h1 className="bg-purple-darker m-6 p-6 rounded shadow-lg">MealPlanner</h1>
-      </header>
-      <p className="text-base">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-      <ul>
-        <li>
-          <Link to="/ingredients">Ingredients</Link>
-        </li>
-        <li>
-          <Link to="/recipes">Recipes</Link>
-        </li>
-      </ul>
-
-      <Route exact path="/" component={Home} />
-      <Route path="/ingredients" component={Ingredients} />
-      <Route path="/recipes" component={Recipes} />
-    </div>
-  </Router>
+  <div className="lg:container mx-auto">
+    <Router>
+      <div>
+        <header className="App-header">
+          <h1 className="bg-purple-darker text-grey-lightest p-6 shadow-lg">MealPlanner</h1>
+        </header>
+        <p className="text-base p-6">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <ul>
+          <li>
+            <Link to="/ingredients">Ingredients</Link>
+          </li>
+          <li>
+            <Link to="/recipes">Recipes</Link>
+          </li>
+        </ul>
+        <div className="m-6 p-6 container">
+          <Route exact path="/" component={Home} />
+          <Route path="/ingredients" component={Ingredients} />
+          <Route path="/recipes" component={Recipes} />
+        </div>
+      </div>
+    </Router>
+  </div>
 );
 
 export default App;
