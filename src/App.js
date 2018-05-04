9a52ad7a3b290d7ec003b5c459996dc00a011394
James Sral <jamessral@gmail.com>
2018-05-04 11:34:08 -0400

    Fix tailwindcss config

--- src/App.js
+++ src/App.js
@@ -27,11 +27,11 @@ const Recipes = () => (
 
 const App = () => (
   <Router>
-    <div className="App">
+    <div className="text-center">
       <header className="App-header">
-        <h1 className="App-title">MealPlanner</h1>
+        <h1 className="bg-purple-darker m-6 p-6 rounded shadow-lg">MealPlanner</h1>
       </header>
-      <p className="App-intro">
+      <p className="text-base">
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
       <ul>
