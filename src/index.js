69c189b2380f1463dcf05e26dad3db93adedf779
James Sral <jsral@it-mbp-jsral.corp.primedia.com>
2018-06-05 12:43:17 -0400

    Add react-apollo and related dependencies

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { HttpLink } from 'apollo-http-link';
+import { InMemoryCache } from 'apollo-cache-memory';
 
 import './index.css';
 import './styles/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  link: new HttpLink(),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
